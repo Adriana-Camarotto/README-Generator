3ad00c232875207ec899242cb7f2946ec58b3605
Adriana Camarotto <adriana.camarotto@gmail.com>
2023-02-19 19:43:42 +0000

    added array of questions for user

--- index.js
+++ index.js
@@ -1,10 +1,67 @@
 const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
+const utils = require("./utils");
+
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Tabel of Contentes?",
+    },
+    {
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "What are the usage information?",
+        name: "usage",
+
+    },
+    {
+        type: "input",
+        message: "what license is being used? (ie...MIT)",
+        name: "license",     
+    },
+    {
+        type: "input",
+        message: "Who contributed to this project?",
+        name: "contribution",
+    },
+    {
+        type: "input",
+        message: "What is the test instructions?",
+        name:  "testInstructions",
+    },
+    {
+        type: "input",
+        message: "Contact info for inquires.",
+        name: "Questions",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",    
+    },
 
 ];
 
